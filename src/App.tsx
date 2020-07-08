4128f6b6d0f645039c65ac2236899b422f03b84b
FranLyrio <lyriofranciele@gmail.com>
2020-07-08 16:28:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from '../src/routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+)
+
+export default App;
