25a2fac323afb063afcdcf440238bf602d417777
FranLyrio <lyriofranciele@gmail.com>
2020-07-08 20:04:33 -0300

    :construction: chore: autentica busca por repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg'
 
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 
 interface Repository {
     full_name: string;
@@ -16,19 +16,29 @@ interface Repository {
 }
 
 const Dashboard: React.FC = () => {
+    const [inputError, setInputError] = useState('');
     const [newRepo, setNewRepo] = useState('');
     const [repositories, setRepositories] = useState<Repository[]>([]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
 
-        const response = await api.get<Repository>(`repos/${newRepo}`);
+        if(!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
 
-        const repository = response.data;
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
 
-        setRepositories([...repositories, repository]);
+            const repository = response.data;
 
-        setNewRepo('');
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (error) {
+            setInputError('Erro na busca por esse repositório')
+        }
     }
 
 
@@ -37,13 +47,18 @@ const Dashboard: React.FC = () => {
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form onSubmit={handleAddRepository} >
+            <Form
+            hasError={!!inputError}
+            onSubmit={handleAddRepository}
+            >
                 <input
                 value={newRepo}
                 onChange={(event) => setNewRepo(event.target.value)}
                 placeholder="Digite usuário/repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
+
+            {inputError && <Error>{inputError}</Error>}
 
             <Repositories>
                 {repositories.map(repository => (
